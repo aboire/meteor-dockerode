8370e0b2c8cccd9dae6e40d95de2b65e6cc6058a
aaronjudd <aaronjudd@me.com>
2014-08-07 09:23:03 -0700

    Update dockerode version and meteor package description

--- package.js
+++ package.js
@@ -1,13 +1,15 @@
 Package.describe({
   name: "dockerode",
-  summary: "Wraps the dockerode package for Meteor"
+  summary: "Docker remote API. Wraps the dockerode package for Meteor.",
+  version: "0.1.0",
+  git: "https://github.com/ongoworks/meteor-dockerode"
 });
 
 Npm.depends({
-  "dockerode": "1.3.0"
+  "dockerode": "2.0.1"
 });
 
-Package.on_use(function (api) {  
+Package.on_use(function (api) {
   api.export('Docker');
   api.add_files('dockerode.js', 'server');
 });
