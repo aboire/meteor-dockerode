9a4d720b6ead04f79fc4123f12bd916746f5278c
Aaron Judd <aaron@ongoworks.com>
2014-09-25 11:04:04 -0700

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 /* Meteor package information for package Dockerode*/
 Package.describe({
   summary: "Docker remote API. Wraps the dockerode package for Meteor.",
+  name: "ongoworks:dockerode",
   version: "0.1.2",
   git: "https://github.com/ongoworks/meteor-dockerode"
 });
