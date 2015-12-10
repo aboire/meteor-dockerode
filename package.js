961bc73b88d8abcdc62fc9684d332f9f072adce0
Thomas Craipeau <thomas.craipeau@gmail.com>
2015-12-10 10:02:38 +0400

    update package dockernode 2.2.7

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 /* Meteor package information for package Dockerode*/
 Package.describe({
-  name: "ongoworks:dockerode",
+  name: "aboire:dockerode",
   summary: "Docker remote API. Wraps the dockerode package for Meteor.",
-  version: "2.2.3",
+  version: "2.2.7",
   git: "https://github.com/ongoworks/meteor-dockerode"
 });
 
 Npm.depends({
-  "dockerode": "2.2.3"
+  "dockerode": "2.2.7"
 });
 
 Package.on_use(function (api) {
